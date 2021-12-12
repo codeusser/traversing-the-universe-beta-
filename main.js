447dd0ef2295fd3c5cd757ea9ad3ec6ef7da477e
codeusser <vladnistor843@gmail.com>
2021-12-13 00:29:05 +0300

    BIGGEST CHANGE
    Am adaugat pamantul si animatie stele

--- main.js
+++ main.js
@@ -1,46 +1,72 @@
-const loader = new THREE.GLTFLoader();
-scene = new THREE.Scene();
-const color3 = new THREE.Color("rgb(216,	216,	230)");
-scene.background = new THREE.Color(color3);
+var stars = [ ]
+const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth,window.innerHeight);
-document.body.appendChild(renderer.domElement);
-camera.position.z= 1;
-var i = 0;
-var _y = 0;
-var _z = 0;
-let loaders = ["super_human/scene.gltf", "pack4/scene.gltf"];
-function inti(u,t){
- 
-loader.load(loaders[i], function(gltf){
-  car = gltf.scene.children[0];
-  car.scale.set(0.5,0.5,0.5);
-  scene.add(gltf.scene);
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
 });
-}
-document.getElementById("3").onclick = function() {
-  while(scene.children.length > 0){ 
-    scene.remove(scene.children[0]); 
-  }
+renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 
-  if(i == 1){
-    camera.position.z = 70;
-    
-  }
-  _y+=0.1;
-  _z+= 0.1;
-  inti(_z,_y);
+camera.position.z = 20;
+// stelele
+
+function addSphere(){
+
   
-  i++;
+  for ( var z= -1000; z < 100; z+=4 ) {
+
+    
+    var geometry   = new THREE.SphereGeometry(0.5, 32, 32)
+    var material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+    var sphere = new THREE.Mesh(geometry, material)
+
+   
+    sphere.position.x = Math.random() * 1000 - 500;
+    sphere.position.y = Math.random() * 1000 - 500;
+
+    
+    sphere.position.z = z;
+
+   
+    sphere.scale.x = sphere.scale.y = 2;
+
+   
+    scene.add( sphere );
+
+
+    stars.push(sphere); 
+  }
 }
+
+function animateStars() { 
+  
+
+  for(var i=0; i<stars.length; i++) {
+
+    star = stars[i]; 
+    star.position.z +=  i/1000;
+    if(star.position.z>1000) star.position.z-=2000; 
+
+    }
+
+}
+
+// pamantul
+const sphere = new THREE.Mesh(
+  new THREE. SphereGeometry(5,50,50),
+  new THREE.MeshBasicMaterial({
+  //color: 0xff0000
+  map: new THREE.TextureLoader().load('index.jpg')
+}));
+scene.add(sphere);
 
 
 function animate(){
-    requestAnimationFrame(animate);
+  sphere.rotation.y += 0.001
+  requestAnimationFrame(animate);
+  
     renderer.render(scene,camera);
-    
-
+    animateStars();
 }
-
+addSphere();
 animate();
